f989987e00850ca3011820360f6ce3a01fd5cabb
DrhSponsor <141895433+DrhSponsor@users.noreply.github.com>
2024-11-18 13:37:30 +0800

    Update tieba.py

--- tieba.py
+++ tieba.py
@@ -10,6 +10,7 @@ params = {
 def get_tieba():
     r = requests.get(url,params=params)
     if r.status_code != 200:
+        print('贴吧错误码',r.status_code)
         return '贴吧访问错误'
     num = bs(r.text, features='lxml').find(class_='card_infoNum').text
     # num是82,404 这样有逗号的
